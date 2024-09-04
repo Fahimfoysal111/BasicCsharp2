6547ab491265c069fa12849bfed51db63583b19a
user <user@DESKTOP-67OG5VN>
2024-09-05 00:59:52 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_19//Enumerating Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Mylist = new List<int>();
+            Mylist.Add(1);
+            Mylist.Add(2);
+            Mylist.Add(3);
+            Mylist.Add(4);
+            Mylist.Add(5);
+            List<int>.Enumerator em = Mylist.GetEnumerator();
+            Display(em);
+        }
+        static void Display(IEnumerator<int> em)
+        {
+            while (em.MoveNext()) 
+            {
+                int bal = em.Current;
+                Console.WriteLine(bal);
+            }
+        }
+         
+    }
+}
